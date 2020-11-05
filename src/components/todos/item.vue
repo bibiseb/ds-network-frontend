9a1b16fa9cff7dab94d78115752097cf0c2de150
bibiseb <hello@ds-network.be>
2020-11-05 16:38:34 +0100

    Remove todo checkbox when user is not allowed to modify

--- src/components/todos/item.vue
+++ src/components/todos/item.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="todos__container">
     <div :class="{ 'todos__todo': true, '-complete': todo.complete }">
-      <input type="checkbox" v-model="todo.complete" @change="$emit('toggle', todo)">
+      <input
+        type="checkbox"
+        v-model="todo.complete"
+        @change="$emit('toggle', todo)" v-if="authenticated && ['ADMINISTRATOR'].includes(user.role)">
       {{ todo.name }}
     </div>
     <div class="todos__ctrl" v-if="authenticated && ['ADMINISTRATOR'].includes(user.role)">
